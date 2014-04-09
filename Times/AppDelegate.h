49bb1770c7259b3c0bbffef7b8eefb897c59b0c0
dtsbourg <dylan.bourgeois@epfl.ch>
2014-04-09 23:31:18 +0200

    Initial Commit

---
+++ Times/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Times
+//
+//  Created by Dylan Bourgeois on 09/04/14.
+//  Copyright (c) 2014 Dylan Bourgeois. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Times/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Times
+//
+//  Created by Dylan Bourgeois on 09/04/14.
+//  Copyright (c) 2014 Dylan Bourgeois. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
